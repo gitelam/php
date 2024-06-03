2de507044a21f107d7e2f0501d45509bbc8af57c
@gitelam <smiotics@gmail.com>
2024-06-03 16:28:51 -0700

    'hi'

---
+++ variables_in_php.php
@@ -0,0 +1,29 @@
+<?php
+
+    $string_variable = "hello world";
+
+    $number_variable = 2;
+
+    $decimal_variable = 23.40099881020930303;
+
+    $simple_col_variable = 'hello world';
+
+    $simple_html_element = "<i class='wup'>I am a php html element...";
+
+    $simple_css_element = "<style>.wup {background-color: #000000; padding: 12px; color: white;}</style>";
+
+    $mid_simple_html_element = "<div style='display: flex; justify-content: center; align-items: center; padding: 12px; background-color: #000000; color: white;'> hello world ! </div>";
+
+    print $mid_simple_html_element;
+
+    print $simple_css_element;
+
+    print $simple_html_element;
+
+    print $simple_html_element;
+
+    print $decimal_variable;
+
+    //hi i am a comment
+    
+?>
